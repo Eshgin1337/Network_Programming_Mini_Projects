e32e2288d95d589f2a904ff9c3fa3a73d1572c9d
Eshgin1337 <eshgin.hasanov.std@bhos.edu.az>
2023-07-20 23:18:40 +0400

    readme for todolist ready

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Web-project
+# Client_Server_TodoList
+<b>Client_Server_TodoList</b> is a simple client-server application for managing a todo list. The project consists of two main components: the server and the client. The server handles incoming client requests, while the client interacts with the server to add and retrieve todo list items.
+
+## Features
+<ul>
+    <li>Add items to the todo list using the client.</li>
+    <li>Retrieve the current todo list from the server using the client.</li>
+    <li>End the session gracefully and save the todo list to a file on the server.</li>
+</ul>
+
+## Requirements
+<ul>
+    <li>Python 3.x</li>
+    <li>The socket module (standard library)</li>
+</ul>
+
+Getting Started
+## Clone the repository to your local machine:
+<ol type=1>
+    <li>
+        Clone the repository to your local machine: <br>
+        <b>
+            git clone https://github.com/your-username/Client_Server_TodoList.git <br> 
+            cd Client_Server_TodoList
+        </b>
+    </li>
+    <li>
+        Make sure you have Python 3.x installed. If not, you can download it from the official Python website:
+    </li>
+</ol>
+
+## Server
+The server is responsible for handling client connections and managing the todo list.
+
+## How to Run the Server
+To run the server, execute the following command in the terminal: <br>
+<b>python3 todolist.py server</b> <br>
+The server will start listening on port 3000 for incoming client connections.
+
+## Client
+The client allows users to interact with the server and manage the todo list.
+
+## How to Run the Client
+To run the client, execute the following command in the terminal: <br>
+<b>python3 todolist.py client</b> <br>
+The client will connect to the server at the specified address (by default, it connects to '127.0.0.1' on port 3000).
+
+## Usage
+<ul>
+    <li>`Type start_list_session;` to begin the session and start adding items to the todo list.</li>
+    <li>`Type insert list item: [your todo item]` to add an item to the list.</li>
+    <li>`Type show_list;` to retrieve the current todo list from the server and display it.</li>
+    <li>`Type end_list_session;` to end the session and save the todo list to the server.</li>
+</ul>
+
+# Contributing
+Contributions are welcome! If you find any issues or have suggestions for improvements, please create a new issue or submit a pull request.
--- ToDoList-using/todolist.py
+++ ToDoList-using/todolist.py
@@ -13,63 +13,84 @@ def server_socket_func(address):
     server_socket.listen(5)
     print("[*] Server started listening on port 3000...")
     while True:
-        client, addr = server_socket.accept()
-        print(f"Got connection from {addr}")
-        new_msg = True
-        full_msg = ""
-        try:
-            while True:
-                msg = client.recv(16)
-                if new_msg:
-                    msg_len = int(msg[:HEADERSIZE])
-                    new_msg = False
-                full_msg += msg.decode('utf-8')
-                if len(full_msg) - HEADERSIZE == msg_len:
-                    main_msg = full_msg[HEADERSIZE:]
-                    if main_msg == "show_list;":
-                        msg_to_client = pickle.dumps(list_items)
-                        encoded_msg = f"{len(msg_to_client) :< {HEADERSIZE}}".encode('utf-8') + msg_to_client
-                        client.send(encoded_msg)
-                    else:
-                        list_items.append(main_msg)
-                    full_msg = ""
-                    new_msg = True
-        except:
-            sys.exit(0)
+        with open("list_items.txt", "a") as myfile:
+            client, addr = server_socket.accept()
+            print(f"Got connection from {addr}")
+            new_msg = True
+            full_msg = ""
+            try:
+                while True:
+                    msg = client.recv(16)
+                    if new_msg:
+                        msg_len = int(msg[:HEADERSIZE])
+                        new_msg = False
+                    full_msg += msg.decode('utf-8')
+                    if len(full_msg) - HEADERSIZE == msg_len:
+                        main_msg = full_msg[HEADERSIZE:]
+                        if main_msg == "show_list;":
+                            msg_to_client = pickle.dumps(list_items)
+                            encoded_msg = f"{len(msg_to_client) :< {HEADERSIZE}}".encode('utf-8') + msg_to_client
+                            client.send(encoded_msg)
+                        else:
+                            list_items.append(main_msg)
+                        full_msg = ""
+                        new_msg = True
+            except:
+                for item in list_items:
+                    if item != "end_list_session;":
+                        myfile.write(item + "\n")
+                sys.exit(0)
 
 
 def client_socket_func(address):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(address)
 
-    client_command = input("$ ")
-    if client_command == "start_list_session;":
-        on_session = True
-        while on_session:
-            list_item = input("insert list item: ")
+    print("Client connected to the server.")
+    print("Type 'start_list_session;' to begin.")
+    on_session = True
 
-            if list_item == "end_list_session;":
-                client_socket.close()
-                print("Session ended successfully!")
-                sys.exit(0)
-            else:
-                sized_list_item = f"{len(list_item) :< {HEADERSIZE}}" + list_item
-                client_socket.send(sized_list_item.encode('utf-8'))
-                if list_item == "show_list;":
-                    got_msg = False
-                    got_size = False
-                    accepted_msg = b''
-                    while not got_msg:
-                        msg = client_socket.recv(16)
-                        if not got_size:
-                            msg_len = int(msg[:HEADERSIZE])
-                            got_size = True
-                        accepted_msg += msg
-                        if len(accepted_msg) - HEADERSIZE == msg_len:
-                            accepted_msg = pickle.loads(accepted_msg[HEADERSIZE:])
-                            for item in accepted_msg:
-                                print("  * " + item)
-                            got_msg = True
+    while on_session:
+        client_command = input("$ ")
+
+        if client_command == "start_list_session;":
+            list_items = []
+            while True:
+                list_item = input("insert list item: ")
+
+                if list_item == "end_list_session;":
+                    sized_list_item = f"{len(list_item):<{HEADERSIZE}}" + \
+                        list_item
+                    client_socket.send(sized_list_item.encode('utf-8'))
+                    client_socket.close()
+                    print("Session ended successfully!")
+                    sys.exit(0)
+                else:
+                    sized_list_item = f"{len(list_item):<{HEADERSIZE}}" + \
+                        list_item
+                    client_socket.send(sized_list_item.encode('utf-8'))
+                    if list_item == "show_list;":
+                        received_msg = b''
+                        new_msg = True
+
+                        while True:
+                            msg = client_socket.recv(16)
+                            if new_msg:
+                                msg_len = int(msg[:HEADERSIZE])
+                                new_msg = False
+                            received_msg += msg
+
+                            if len(received_msg) - HEADERSIZE == msg_len:
+                                decoded_msg = received_msg[HEADERSIZE:]
+                                accepted_msg = pickle.loads(decoded_msg)
+                                for item in accepted_msg:
+                                    print("  * " + item)
+                                new_msg = True
+                                received_msg = b''
+                                break
+        else:
+            print("Invalid command. Type 'start_list_session;' to begin.")
+
 
 
 if len(sys.argv) < 2:
